e085b000d2ff0ac8a5e7bd3fdee8483b1d0067ef
Jordy <jbonn2002@gmail.com>
2023-03-08 21:18:09 -0800

    added toggle navbar

--- main.js
+++ main.js
@@ -186,4 +186,21 @@ window.addEventListener('resize', onWindowResize, false)
 
 animate()
 
+//TOGGLE NAVBAR CODE
+
+const toggleButton = document.querySelector('.toggle-button')
+const toggleButtonIcon = document.querySelector('.toggle-button i')
+const dropDownMenu =  document.querySelector('.dropdown-menu')
+
+toggleButton.onclick = function () {
+  dropDownMenu.classList.toggle('open')
+  const isOpen = dropDownMenu.classList.contains('open')
+
+  toggleButtonIcon.classList = isOpen
+  ? 'fa-solid fa-xmark'
+  : 'fa-solid fa-bars'
+}
+
+
+
 
